62ed328543f85039fa095b4e4fd609eb7c9b95bf
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-10-12 13:41:40 +0600

    login and register page font updated

--- src/app/iniciar-sesion/page.jsx
+++ src/app/iniciar-sesion/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
             <div className='pt-20  flex gap-0 h-screen'>
                 <div className='hidden lg:grid w-[30%] bg-[#4021C8]'>
                     <div className='pl-20 pt-24'>
-                        <h3 className='text-white text-3xl font-extrabold w-80'>Recupera el control de tus finanzas</h3>
+                        <h3 className='text-white text-3xl font-extrabold w-80 font-dm_sans'>Recupera el control de tus finanzas</h3>
                         <Image src="/register.png" width="500" height="500" alt="" className='' />
                     </div>
                 </div>
@@ -18,18 +18,18 @@ const page = () => {
                     <div className="flex flex-col border-opacity-50" >
                         <div>
                             <div className='w-[80%] md:w-full mx-auto'>
-                                <h3 className='text-2xl lg:text-4xl font-bold'>Accede a tu cuenta</h3>
-                                <p className='text-lg text-[#9b9a9a] mt-5 mb-10 md:w-[80%] md:mx-auto'>Inicia sesión con tu cuenta de Google, con tu email o con un código de un solo uso.</p>
-                                <button className='btn bg-white rounded-full text-sm w-full lg:w-[80%] mx-auto'><FcGoogle className='text-xl relative -left-12 md:-left-2 lg:-left-40' />Registrarse con Google</button>
-                                <button className='btn bg-white rounded-full text-sm w-full lg:w-[80%] mx-auto mt-5 text-center'>*** Solicitar código de un uso</button>
+                                <h3 className='text-2xl lg:text-4xl font-bold font-montserrat'>Accede a tu cuenta</h3>
+                                <p className='text-lg text-[#9b9a9a] mt-5 mb-10 md:w-[80%] md:mx-auto font-dm_sans'>Inicia sesión con tu cuenta de Google, con tu email o con un código de un solo uso.</p>
+                                <button className='btn bg-white rounded-full text-sm w-full lg:w-[80%] mx-auto font-dm_sans'><FcGoogle className='text-xl relative -left-12 md:-left-2 lg:-left-40' />Registrarse con Google</button>
+                                <button className='btn bg-white rounded-full text-sm w-full lg:w-[80%] mx-auto mt-5 text-center font-dm_sans'>*** Solicitar código de un uso</button>
                             </div>
                         </div>
-                        <div className="divider my-10 w-[80%] md:w-full lg:w-[80%] mx-auto">O</div>
+                        <div className="divider my-10 w-[80%] md:w-full lg:w-[80%] mx-auto font-dm_sans">O</div>
                         <div className='space-y-3 w-[80%] md:w-full mx-auto'>
-                            <input type="email" placeholder="Tu correo electrónico" className="input input-bordered w-full lg:w-[80%] " />
-                            <input type="password" placeholder="Contraseña" className="input input-bordered w-full lg:w-[80%]" />
+                            <input type="email" placeholder="Tu correo electrónico" className="input input-bordered w-full lg:w-[80%] font-dm_sans" />
+                            <input type="password" placeholder="Contraseña" className="input input-bordered w-full lg:w-[80%] font-dm_sans" />
                             <div className='py-5'>
-                                <button className='btn btn-lg text-white rounded-full w-full lg:w-[80%] bg-[#4021C8] '>Empezar</button>
+                                <button className='btn btn-lg text-white rounded-full w-full lg:w-[80%] bg-[#4021C8] font-montserrat'>Empezar</button>
                             </div>
                         </div>
                     </div>
--- src/app/registrarse/page.jsx
+++ src/app/registrarse/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
             <div className='pt-20  flex gap-0 h-screen'>
                 <div className='hidden lg:grid w-[30%] bg-[#4021C8]'>
                     <div className='pl-20 pt-24'>
-                        <h3 className='text-white text-3xl font-extrabold w-80'>Recupera el control de tus finanzas</h3>
+                        <h3 className='text-white text-3xl font-extrabold w-80 font-dm_sans'>Recupera el control de tus finanzas</h3>
                         <Image src="/register.png" width="500" height="500" alt="" className='' />
                     </div>
                 </div>
@@ -18,17 +18,17 @@ const page = () => {
                     <div className="flex flex-col border-opacity-50" >
                         <div>
                             <div className='w-[100%] mx-auto'>
-                                <h3 className='text-2xl md:text-3xl lg:text-4xl font-bold'>Empieza tu prueba gratis</h3>
-                                <p className='md:text-lg lg:text-lg text-[#9b9a9a] mt-5 mb-10'>Prueba de 14 días. No necesitas tarjeta de crédito.</p>
-                                <button className='w-[90%] md:w-[100%] btn bg-white rounded-full text-sm mx-auto'><FcGoogle className='text-xl relative -left-16 md:-left-28' />Registrarse con Google</button>
+                                <h3 className='text-2xl md:text-3xl lg:text-4xl font-bold font-montserrat'>Empieza tu prueba gratis</h3>
+                                <p className='md:text-lg lg:text-lg text-[#9b9a9a] mt-5 mb-10 font-dm_sans'>Prueba de 14 días. No necesitas tarjeta de crédito.</p>
+                                <button className='w-[90%] md:w-[100%] btn bg-white rounded-full text-sm mx-auto font-dm_sans'><FcGoogle className='text-xl relative -left-16 md:-left-28' />Registrarse con Google</button>
                             </div>
                         </div>
-                        <div className="divider my-10 w-[90%] md:w-[100%] mx-auto">O</div>
+                        <div className="divider my-10 w-[90%] md:w-[100%] mx-auto font-dm_sans">O</div>
                         <div className='space-y-3 w-[90%] md:w-full mx-auto'>
-                            <input type="email" placeholder="Tu correo electrónico" className="input input-bordered w-full lg:w-full " />
-                            <input type="password" placeholder="Contraseña" className="input input-bordered w-full lg:w-full" />
+                            <input type="email" placeholder="Tu correo electrónico" className="input input-bordered w-full lg:w-full font-dm_sans" />
+                            <input type="password" placeholder="Contraseña" className="input input-bordered w-full lg:w-full font-dm_sans" />
                             <div className='py-5'>
-                                <button className='btn btn-lg text-white rounded-full w-full bg-[#4021C8] '>Empezar</button>
+                                <button className='btn btn-lg text-white rounded-full w-full bg-[#4021C8] font-montserrat'>Empezar</button>
                             </div>
                         </div>
 
